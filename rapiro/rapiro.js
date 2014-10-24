df7c59386d9ff301be59606483a28a2e01395e78
Junichi Kobayashi <nnujabok@gmail.com>
2014-10-24 11:52:21 +0900

    Reindent rapiro.js

--- rapiro/rapiro.js
+++ rapiro/rapiro.js
@@ -1,4 +1,4 @@
-var Cylon = require('cylon');
+  var Cylon = require('cylon');
 
 // Initialize the robot
 Cylon.robot({
@@ -10,13 +10,40 @@ Cylon.robot({
 
     console.log("forward");
 
-    every(1..second(), function() {
-      if (my['doneWalking'] == false) {
-        my.rapiro.forward();
+    every((3).seconds(), function() {
+      // if (my['doneWalking'] == false && on == false) {
+      // my.rapiro.led(0,0,0);
+      // on = true;
+      // }
+      // else if(my['doneWalking'] == false && on == true){
+      // my.rapiro.led(255,0,0);
+      // on = false;
+      // }
+      // if(my['doneWalking'] == false){
+      // my.rapiro.concerned();
+      // }
+      if(happy == false){
+        my.rapiro.happy();
+        happy = true;
+      }
+      else if(unhappy == false){
+        my.rapiro.unhappy();
+        unhappy = true;
+      }
+      else if(concerned == false){
+        my.rapiro.concerned();
+        concerned = true;
+      }
+      else if(mad == false){
+        my.rapiro.mad();
+        mad = true;
+        my.rapiro.rightShoulderPitch(90);
       }
     });
     after(10..seconds(), function() {
       console.log("halt");
+      console.log("rightshold_p");
+      // my.rapiro.rightShoulderPitch(90);
       my.rapiro.stop();
       my['doneWalking'] = true;
     });
